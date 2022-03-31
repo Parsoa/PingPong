93defb7b61aad80dc69e13dcec7d116ee84540c2
Luca Denti <ldenti92@gmail.com>
2022-03-31 19:04:55 +0200

    SFS conversion directly from .sfs file (no .fq needed) * converter has not been updated to work well with latest .sfs version

--- converter.cpp
+++ converter.cpp
@@ -3,49 +3,34 @@
 KSEQ_INIT(gzFile, gzread)
 
 void Converter::run() {
-    auto c = Configuration::getInstance();
-    string fq_path = c->fastq;
-    string sfs_path = c->bed;
-    int tau = c->cutoff;
+  auto c = Configuration::getInstance();
+  string sfs_path = c->bed;
 
-    // 1 Parsing and storing sfss
-    map<string, vector<SFS>> SFSs = parse_sfsfile(sfs_path, tau);
-
-    // 2 Parsing reads
-    map<string, uint> allsfs;
-    gzFile fp = gzopen(fq_path.c_str(), "r");
-    kseq_t *seq = kseq_init(fp);
-    int l;
-    uint idx = 0;
-    while ((l = kseq_read(seq)) >= 0) {
-        string header = seq->name.s;
-        if (SFSs.find(header) == SFSs.end()) {
-        // cerr << "Skipping " << header << endl;
-            continue;
-        }
-        for (const SFS &sfs : SFSs.at(header)) {
-            uint st = sfs.s;
-            uint len = sfs.l;
-            uint c = sfs.c;
-            string sfsseq(seq->seq.s, st, len);
-            string rcsfsseq(sfsseq);
-            for (uint j = 0; j < sfsseq.size(); ++j) {
-                rcsfsseq[j] = RCN[sfsseq[j]];
-            }
-            reverse(rcsfsseq.begin(), rcsfsseq.end());
-            sfsseq = min(sfsseq, rcsfsseq);
-
-            string qual(len, '-');
-            cout << "@" << header << "#" << idx << "#" << st << "#" << st + len - 1
-                << "#" << c << "\n"
-                << sfsseq << "\n"
-                << "+"
-                << "\n"
-                << qual << endl;
-            ++idx ;
-        }
+  string line;
+  ifstream inf(sfs_path);
+  int idx = 1;
+  if (inf.is_open()) {
+    string info[5];
+    string read_name;
+    while (getline(inf, line)) {
+      stringstream ssin(line);
+      int i = 0;
+      while (ssin.good() && i < 5)
+	ssin >> info[i++];
+      if (info[0].compare("*") != 0) {
+	read_name = info[0];
+	idx = 1;
+      }
+      string seq = info[1];
+      int s = stoi(info[2]);
+      int l = stoi(info[3]);
+      string qual (l, '~');
+      cout << "@" << read_name << "#" << idx << "#" << s << "#" << s + l - 1 << "\n"
+	   << seq << "\n"
+	   << "+"
+	   << "\n"
+	   << qual << endl;
+      ++idx ;
     }
-
-    kseq_destroy(seq);
-    gzclose(fp);
+  }
 }
